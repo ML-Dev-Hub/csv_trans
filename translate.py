36fac26890da2318bfc2e5ab6d8d264095d8447a
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-03-12 17:57:44 +0900

    updated the code for argument parsing and made changes in the utils

--- translate.py
+++ translate.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 from utils import read_csv_file, find_encoding_scheme, api_translate, save_csv_file
+import tqdm
 
-
-def translate(file, target_language, sep=','):
+def translate(file, source_language, target_language, sep=','):
     """
     Input: file, target_language
     Output: translated data
@@ -15,21 +15,28 @@ def translate(file, target_language, sep=','):
     ## read the csv file
     data = read_csv_file(file, encoding_scheme, sep=sep)
     ## translate the data
-    for col in data.columns:
-        data[col] = data[col].apply(lambda x: api_translate(x, target_language))
+    for col in tqdm.tqdm(data.columns):
+        data[col] = data[col].apply(lambda x: api_translate(x, source_language, target_language))
         
     ## save the data to the csv file
     save_csv_file(data, file, encoding_scheme, sep=sep)
-    return data
+    
 
+
+
+## create a function that takes arguments as file path, target language, file seperator using cmd and parameters
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_path', type=str, required=True, help='file path')
+    parser.add_argument('--file_sperator', type=str, required=True, help='file seperator')
+    parser.add_argument('--source_language', type=str, required=True, help='source language')
+    parser.add_argument('--target_language', type=str, required=True, help='target language')
+    args = parser.parse_args()
+    translate(args.file_path, args.source_language ,args.target_language, sep=args.file_sperator)
 
 
 if __name__ == "__main__":
-    #file path
-    file_path = "urdu_headlines.csv"
-    file_sperator = ","
-    target_language = "en"
-    
-    translate_data = translate(file_path, target_language, sep=file_sperator)
+    main()
     
     
--- utils.py
+++ utils.py
@@ -1,17 +1,57 @@
 import pandas as pd
 import numpy as np
 import time
+import random
 
 import chardet
-from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator, MyMemoryTranslator
 
 ## turn off warning
 import warnings
 warnings.filterwarnings("ignore")
 
+## write a function a change IP address of the system also import the necessary libraries
+def change_ip_address():
+    """
+    Input: None
+    Output: None
+    
+    Description: This function will change the IP address of the system
+    """
+    import os
+    os.system("ipconfig /release")
+    os.system("ipconfig /renew")
+    os.system("ipconfig /flushdns")
+    os.system("ipconfig /registerdns")
+    os.system("ipconfig /renew6")
+    os.system("ipconfig /flushdns")
+
+
+## spliting the data to chunks of 4000 characters
+def split_text(text):
+    chunks = []
+    start = 0
+    end = 4000
+
+    while start < len(text):
+        if end >= len(text):
+            chunks.append(text[start:])
+            break
+
+        while text[end] != ' ' and end > start + 4000 - 10:
+            end -= 1
+
+        if end <= start:
+            end = start + 4000
+
+        chunks.append(text[start:end])
+        start = end + 1
+        end = start + 4000
+
+    return chunks
 
 ## api for translation the values in the column
-def api_translate(data, target_language):
+def api_translate(data, source_language, target_language):
     ## write dog string to the file
     """
     Input: data, target_language
@@ -20,11 +60,21 @@ def api_translate(data, target_language):
     Description: This function will translate the data into the target language using google translator
     """
     try:
-        translated = GoogleTranslator(source='auto', target=target_language).translate(data)
+        random_seed = random.randint(1, 3)
+        time.sleep(random_seed)
+        
+        if len(data) < 4000:
+            translated = GoogleTranslator(source=source_language, target=target_language).translate(data)
+        else:
+            ## split the data into 4000 characters while ensuring that the last word is space
+            split_data = split_text(data)
+            translated = ''
+            for i in split_data:
+                translated += GoogleTranslator(source=source_language, target=target_language).translate(i)      
     except:
-        ##sleep for 2 seconds
-        time.sleep(2)
-        translated = GoogleTranslator(source='auto', target=target_language).translate(data)
+        ## print the catch error
+        print("Error in translating the data")
+        print("type error: " + str(TypeError))
     return translated
 
 ### reading any csv file, using different encoding scheme
